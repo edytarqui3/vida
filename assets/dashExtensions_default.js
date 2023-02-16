fbf00533b66e283be8cf69e57d2121ead8f53dcd
Edy Tarqui <etarqui2.mega.nz@gmail.com>
2023-02-16 07:04:32 -0400

    logos

---
+++ assets/dashExtensions_default.js
@@ -0,0 +1,19 @@
+window.dashExtensions = Object.assign({}, window.dashExtensions, {
+    default: {
+        function0: function(feature, context) {
+            return context.props.hideout.filter.includes(feature.properties.CODIGOCONTRATO);
+        },
+        function1: function(feature, latlng, context) {
+            const {
+                min,
+                max,
+                colorscale,
+                circleOptions,
+                colorProp
+            } = context.props.hideout;
+            const csc = chroma.scale(colorscale).domain([min, max]); // chroma lib to construct colorscale
+            circleOptions.fillColor = csc(feature.properties[colorProp]); // set color based on color prop.
+            return L.circleMarker(latlng, circleOptions); // sender a simple circle marker.
+        }
+    }
+});
